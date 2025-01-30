9cd9cc9dcae5ac7f01edd4d8068e66ea050f8e59
Gerry Sebb <gerry@sebb.de>
2025-01-31 00:58:51 +0100

    V 2.4

--- src/NMEA0183Telegram.h
+++ src/NMEA0183Telegram.h
@@ -38,22 +38,38 @@ Transducer Values
 *  $--XDR,a,x.x,a,c--c, ..... *hh<CR><LF> \\
 
     Field Number:
-    1) Transducer Type
-    2) Measurement Data
-    3) Units of measurement
-    4) Name of transducer
-    x) More of the same
-    n) Checksum
+      1) Transducer Type
+      2) Measurement Data
+      3) Units of measurement
+      4) Name of transducer
+      x) More of the same
+      n) Checksum
 
     Example:
     Temperatur $IIXDR,C,19.52,C,TempAir*19
     Druck      $IIXDR,P,1.02481,B,Barometer*29
     Kraengung  $IIXDR,A,0,x.x,ROLL*hh<CR><LF>
+
+
+  RPM - Revolutions
+
+        1 2 3   4   5 6
+        | | |   |   | |
+ $--RPM,a,x,x.x,x.x,A*hh<CR><LF> 
+
+    Field Number: 
+      1) Sourse, S = Shaft, E = Engine
+      2) Engine or shaft number
+      3) Speed, Revolutions per minute
+      4) Propeller pitch, % of maximum, "-" means astern
+      5) Status, A means data is valid
+      6) Checksum 
+
 */
 
 /**
  * @brief Send NMEA0183
- * Send Sensor data
+ * Send XDR Sensor data
  * @return String 
  */
 
@@ -80,3 +96,30 @@ String sendXDR()
   return SendSensor;
 }
 
+/**
+ * @brief Send NMEA0183
+ * Send RPM Sensor data
+ * @return String 
+ */
+
+String sendRPM()
+{   
+  String HexCheckSum;
+  String NMEASensor;
+  String SendSensor;
+  
+    NMEASensor = "IIRPM,E,1,";  //NMEASensor = "IIXDR,E,1," + String(SensorID);  
+    NMEASensor += String(fGaugeDrehzahl);
+    NMEASensor += ",15,A";
+
+  // Build CheckSum
+  HexCheckSum = String(CheckSum(NMEASensor), HEX);
+  // Build complete NMEA string
+  SendSensor = "$" + NMEASensor;
+  SendSensor += "*";
+  SendSensor += HexCheckSum;
+
+  Serial.println(SendSensor);
+  
+  return SendSensor;
+}
